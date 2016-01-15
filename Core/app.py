245d2c4a3180ae3db5faf452bf16f5fd058f0d11
Andrew Codispoti <andrewcod749@gmail.com>
2016-01-14 21:04:11 -0800

    Updated templates.

--- Core/app.py
+++ Core/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+import db_interactor
 app = Flask(__name__)
 
+#handle serving the gui
 @app.route("/")
 def handleHomepage():
     pass
 
+#handle data requests
+@app.route("/data")
+    pass
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
