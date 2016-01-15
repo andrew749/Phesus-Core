6cbcf9bbb969d71ca4b7111513755c3ff43bab2b
Andrew Codispoti <andrewcod749@gmail.com>
2016-01-14 21:24:26 -0800

    Added database connection.

--- Core/db_interactor.py
+++ Core/db_interactor.py
@@ -1,3 +1,11 @@
 #Database adapter
 import psycopg2
+import os
 
+DB_NAME =  os.environ['USER']
+DB_HOST =  os.environ['localhost']
+DB_PASS =  os.environ['PASSWORD']
+try:
+    conn = psycopg2.connect("dbname='template1' user='%s' host='%s' password='%s'")
+except:
+    print "I am unable to connect to the database"
