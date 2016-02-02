c0eb9c3ce036fad9a0cd6a8cb5d27d035814889a
Andrew Codispoti <andrewcod749@gmail.com>
2016-02-01 22:31:42 -0800

    built out handler for real time socket communcication.

--- Core/sockets.py
+++ Core/sockets.py
@@ -1,19 +1,50 @@
 import asyncio
 import websockets
+import json
 """
 Simple python sockets server to handle requests.
 """
-async def hello(websocket, path):
+async def requestHandler(websocket, path):
     while True:
-        print("running")
-        name = await websocket.recv()
-        print("< {}".format(name))
+        print ("Waiting for request")
+        jsonData = await websocket.recv()
+        print ("got request")
+        data = json.loads(jsonData)
+        print ("deserialized data")
+        result = route(data)
+        await websocket.send(result)
+        print ("sent response")
 
-        greeting = "Hello {}!".format(name)
-        await websocket.send(greeting)
-        print("> {}".format(greeting))
+def route(body):
+    """
+    Args:
+        body : json in the form of a dictionary
+    """
+    type = body.type
+    result = None
+    if (type == "create_node"):
+        pass
+    elif (type == "create_user"):
+        pass
+    elif (type == "create_connection"):
+        pass
+    elif (type == "update_node"):
+        pass
+    elif (type == "update_connection"):
+        pass
+    elif (type == "delete_node"):
+        pass
+    elif (type == "delete_connection"):
+        pass
+    elif (type == "delete_graph"):
+        pass
+    elif (type == "create_graph"):
+        pass
+    else:
+        raise PhesusException("Error : command not found")
+    return result
 
-start_server = websockets.serve(hello, 'localhost', 5678)
-
+"""Start running the server."""
+start_server = websockets.serve(requestHandler, 'localhost', 5678)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
